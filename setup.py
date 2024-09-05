e634a44bcd4ce9e4212ffe39470eed1d6891db77
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2024-09-05 19:57:54 +0500

    Added packages in project. Created other files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqGenrator',
+    version='0.0.1',
+    author='Mubassim Ahmed Khan',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
