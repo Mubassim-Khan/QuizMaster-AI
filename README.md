321bceb05d80dfc41b934206a9a0e9a48e86bf85
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2024-09-07 02:09:39 +0500

    Added Preview Image Made some other changes too.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 To create environment use `conda create -p venv python=3.x -y`
 To activate environment use `source activate ./venv` or `conda activate ./venv`
-To run the project `streamlit run StreamlitApp.py`
+To run the project `streamlit run StreamlitApp.py` or `python -m streamlit run StreamlitApp.py`
--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='mcqGenrator',
     version='0.0.1',
     author='Mubassim Ahmed Khan',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","langchain-community","pypdf"],
+    install_requires=["openai","langchain","streamlit","python-dotenv","langchain-community","pypdf"],
     packages=find_packages()
 )
